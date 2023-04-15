c50ff5d051b05bfdd84bb9f479a7318b454d20cc
talzaken <talzaken@gmail.com>
2023-04-15 15:32:52 -0400

    Improve error handling

--- telephonish_be/game/views.py
+++ telephonish_be/game/views.py
@@ -2,7 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RoomSerializer
+from rest_framework.exceptions import APIException
 
+class RoomAlreadyExistsError(APIException):
+    status_code = 403
+    default_detail = 'room_already_exists'
 
 class RoomCreateView(APIView):
 
@@ -11,5 +15,10 @@ class RoomCreateView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        print(serializer.errors)
+
+        if 'room_name' in serializer.errors:
+            if serializer.errors['room_name'][0].code == 'unique':
+                raise RoomAlreadyExistsError
+            
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
