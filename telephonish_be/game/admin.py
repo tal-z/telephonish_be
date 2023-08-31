3b0e88123c297488a27d2e0452703dbba3da8dff
talzaken <talzaken@gmail.com>
2023-08-30 21:27:45 -0400

    make admin easier to read

--- telephonish_be/game/admin.py
+++ telephonish_be/game/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from .models.player import Player
-from .models.prompt import Prompt
-from .models.response import Response
 from .models.room import Room
-from .models.round import Round
 from .models.drawing import Drawing
 from .models.story import Story
 from .models.poem import Poem
@@ -12,52 +9,51 @@ from .models.poem import Poem
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'emoji', 'room', 'is_spectator')
+    list_display = ('id', 'name', 'emoji', 'room', 'is_spectator')
 
 
 @admin.register(Drawing)
 class DrawingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'player', 'room', 'round_number', 'display_dataurl')
+    list_display = ('id', 'player_name', 'room', 'round_number', 'display_dataurl')
 
     def display_dataurl(self, obj):
         return format_html('<img src="{}" width="100" height="100" />', obj.dataUrl)
 
     display_dataurl.short_description = 'Data URL'
 
+    def player_name(self, obj):
+        return obj.player.name
 
 @admin.register(Story)
 class StoryAdmin(admin.ModelAdmin):
     list_display = (
-        "player",
+        'id',
+        "player_name",
         "room",
+        "round_number",
         "story",
     )
+
+    def player_name(self, obj):
+        return obj.player.name
 
 
 @admin.register(Poem)
 class PoemAdmin(admin.ModelAdmin):
     list_display = (
-        "player",
+        'id',
+        "player_name",
         "room",
+        "round_number",
         "poem",
     )
 
+    def player_name(self, obj):
+        return obj.player.name
 
-@admin.register(Prompt)
-class PromptAdmin(admin.ModelAdmin):
-    list_display = ('text',)
-
-
-@admin.register(Response)
-class ResponseAdmin(admin.ModelAdmin):
-    list_display = ('round', 'player', 'content', 'format', 'timestamp')
 
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     pass
 
-
-@admin.register(Round)
-class RoundAdmin(admin.ModelAdmin):
-    list_display = ('room', 'prompt', 'format', 'start_time', 'duration_in_seconds')
